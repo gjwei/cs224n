2f2e3557282381d114d362e5b32be00007c66bfe
1449894353 <1449894353@qq.com>
2017-12-02 23:46:58 +0800

    softmax done

--- assignment1/q1_softmax.py
+++ assignment1/q1_softmax.py
@@ -1,5 +1,5 @@
 import numpy as np
-import random
+
 
 def softmax(x):
     """
@@ -19,12 +19,21 @@ def softmax(x):
     You must implement the optimization in problem 1(a) of the 
     written assignment!
     """
-
+    
     ### YOUR CODE HERE
-    raise NotImplementedError
+    if x.ndim > 1:
+        x -= np.max(x, axis=1, keepdims=True)
+        x = np.exp(x)
+        x /= np.sum(x, axis=1, keepdims=True)
+    else:
+        x -= np.max(x)
+        x = np.exp(x)
+        x /= np.sum(x)
+    
     ### END YOUR CODE
     
     return x
+
 
 def test_softmax_basic():
     """
@@ -32,22 +41,23 @@ def test_softmax_basic():
     Warning: these are not exhaustive.
     """
     print "Running basic tests..."
-    test1 = softmax(np.array([1,2]))
+    test1 = softmax(np.array([1, 2]))
     print test1
     assert np.amax(np.fabs(test1 - np.array(
-        [0.26894142,  0.73105858]))) <= 1e-6
-
-    test2 = softmax(np.array([[1001,1002],[3,4]]))
+            [0.26894142, 0.73105858]))) <= 1e-6
+    
+    test2 = softmax(np.array([[1001, 1002], [3, 4]]))
     print test2
     assert np.amax(np.fabs(test2 - np.array(
-        [[0.26894142, 0.73105858], [0.26894142, 0.73105858]]))) <= 1e-6
-
-    test3 = softmax(np.array([[-1001,-1002]]))
+            [[0.26894142, 0.73105858], [0.26894142, 0.73105858]]))) <= 1e-6
+    
+    test3 = softmax(np.array([[-1001, -1002]]))
     print test3
     assert np.amax(np.fabs(test3 - np.array(
-        [0.73105858, 0.26894142]))) <= 1e-6
-
+            [0.73105858, 0.26894142]))) <= 1e-6
+    
     print "You should verify these results!\n"
+
 
 def test_softmax():
     """ 
@@ -58,8 +68,11 @@ def test_softmax():
     """
     print "Running your tests..."
     ### YOUR CODE HERE
-    raise NotImplementedError
-    ### END YOUR CODE  
+    a = np.random.random(size=(100, 49)) * 10
+    result = softmax(a)
+    
+    ### END YOUR CODE
+
 
 if __name__ == "__main__":
     test_softmax_basic()
